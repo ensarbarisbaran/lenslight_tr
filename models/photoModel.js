c0d367e6eed5221e78b870b559f4fa5181dede09
ensarbarisbaran <barissdf123@gmail.com>
2023-12-23 16:25:40 +0300

    deneme

---
+++ models/photoModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose
+const photoSchema = new Schema({
+    name: {
+        type : String,
+        required: true,
+        trim: true
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    uploadAt:{
+        type: Date,
+        default: Date.now
+    },
+});
+
+const Photo = mongoose.model("photo", photoSchema)
+
+export default Photo;
---
+++ routes/photoRoute.js
@@ -0,0 +1,9 @@
+import  express from "express"
+import * as photoController from "../controllers/photoController.js"
+
+const router = express.Router()
+
+router.route("/").get(photoController.createPhoto)
+
+export default router;
+
